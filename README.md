db9083e06c95be40f51639668215159d0e8cbda8
Timothy Kimathi <timsnky@gmail.com>
2019-06-13 15:41:14 +0300

    Update sql init file and read me

--- README.md
+++ README.md
@@ -7,6 +7,15 @@ A simple spring boot movies web service that facilicates the signup, login and s
 <li>Spring Boot 2.1.5</li>
 <li>Spring Security 2.1.5</li>
 <li>JWT Authentication</li>
-<li>JPA and MySql or H2 Database</li>
+<li>JPA and H2 Database</li>
 <li>Swagger Documentation</li>
 <li>Actuator</li>
+<li>HTTPS Redirect</li>
+<li>Jacoco Code Coverage</li>
+</ul>
+
+<p>IntelliJ IDEA used for development
+
+##Running Application
+<p>I have included a movies-0.0.1-SNAPSHOT.jar file on application root
+that you can run using the following JVM command <strong>jar -jar <file-name>.jar</strong></p>
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,30 +1,15 @@
 INSERT INTO `role` (`id`, `name`)
 VALUES
-	(1, 'Admin'),
-	(2, 'Manager'),
-	(3, 'User');
+	(10, 'Admin'),
+	(11, 'Manager'),
+	(12, 'User');
 
 INSERT INTO `rating` (`id`, `rating`)
 VALUES
-	(1, 1),
-	(2, 2),
-	(3, 3),
-	(4, 4),
-	(5, 5);
+	(10, 1),
+	(11, 2),
+	(12, 3),
+	(13, 4),
+	(14, 5);
 
-INSERT INTO `user` (`id`, `email`, `firstname`, `lastname`, `password`)
-VALUES
-	(1, 'timsnky@gmail.com', 'Timothy', 'Kimathi', '$2a$10$jMK6AfI5f0kRkLDExnj20eJhomDsBmZRvnW1xYd0Zyn8PrPjYvJiq');
-
-INSERT INTO `user_roles` (`user_id`, `roles_id`)
-VALUES
-	(1, 3);
-
-INSERT INTO `movie` (`id`, `description`, `recommendation`, `title`, `watched`, `rating_id`, `user_id`)
-VALUES
-	(1, 'Avengers Description', 'Highly recomended', 'Avengers', 1, 1, 1),
-	(2, 'Avengers Description1', 'Medium recomended', 'Avengers2', 0, 2, 1),
-	(3, 'Avengers Description2', 'Lowly recomended', 'Avengers3', 1, 3, 1),
-	(4, 'Avengers Description4', 'Lowly recomended', 'Avengers4', 0, 4, 1),
-	(5, 'Avengers Description5', 'Lowly recomended', 'Avengers5', 1, 5, 1);
 
